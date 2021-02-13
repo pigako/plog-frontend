f19dddc10b6025c3045b5ce0b295134c3a9bb9bd
Toby Baek <50404311+pigako@users.noreply.github.com>
2021-02-13 20:22:13 +0900

    레이아웃 구조

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import Head from "next/head";
+
+import GlobalStyles from "../components/designs/GlobalStyles";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Plog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+            <GlobalStyles />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
