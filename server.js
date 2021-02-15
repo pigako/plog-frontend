06555a05c09da8e101c50442c4274dda9be0a599
pigako <50404311+pigako@users.noreply.github.com>
2021-02-15 17:25:45 +0900

    로깅 라이브러리 추가

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const next = require("next");
+const morgan = require("morgan");
 
 const port = process.env.NODE_ENV === "production" ? 80 : 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -8,6 +9,8 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
+
+    server.use(morgan("dev"));
 
     server.get("/", (req, res) => {
         if (req.hostname == "hc.check") {
